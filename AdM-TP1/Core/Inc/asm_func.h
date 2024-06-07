16212bd884359daaaadca7521393986848ee1e92
ddalbarracin <danield.albarracin@gmail.com>
2024-06-06 21:20:36 -0300

    AdM: TP3
    * working with DSP and compare clock cycles

--- AdM-TP1/Core/Inc/asm_func.h
+++ AdM-TP1/Core/Inc/asm_func.h
@@ -26,4 +26,13 @@ uint32_t asm_max(int32_t * vectorIn, uint32_t longitud);
 void asm_downSample (int32_t * vectorIn, int32_t * vectorOut, uint32_t longitud, uint32_t N);
 void asm_invertir(uint16_t * vector, uint32_t longitud);
 
+/* Clase 7 - Trabajo Practico 3 */
+uint32_t asm_potencia(int16_t * vecIn, uint32_t longitud);
+uint32_t asm_potencia_DSP(int16_t * vecIn, uint32_t longitud);
+void asm_medDif(int8_t *e, int8_t *x, int8_t *y, uint16_t longitud);
+void asm_medDif_DSP(int8_t *e, int8_t *x, int8_t *y, uint16_t longitud);
+void asm_eco(int16_t * signal, int16_t *eco, uint32_t longitud);
+void asm_eco_DSP(int16_t * signal, int16_t *eco, uint32_t longitud);
+
+
 #endif /* INC_ASM_FUNC_H_ */
