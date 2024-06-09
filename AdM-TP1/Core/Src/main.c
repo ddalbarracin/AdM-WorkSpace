c289344cae7e78e26873f38ba9cd1d2e7f04056c
ddalbarracin <danield.albarracin@gmail.com>
2024-06-09 16:43:17 -0300

    AdM: TP3
    * se agregan comentarios en asm_clase6_tp.S

--- AdM-TP1/Core/Src/main.c
+++ AdM-TP1/Core/Src/main.c
@@ -846,7 +846,7 @@ void c_medDif(int8_t * e, int8_t *x, int8_t *y, uint16_t longitud) {
 void c_eco(int16_t * signal, int16_t *eco, uint32_t longitud) {
 
 	// offset[index] = ((offset[ms] * fs[Hz]) / 1000) - 1
-	uint32_t offset = ((20 * 44100) / 1000) - 1;	// Sample = 81
+	uint32_t offset = ((20 * 44100) / 1000) - 1;	// Initial Eco Sample = 81
 
 	uint32_t sample = 0;
 
