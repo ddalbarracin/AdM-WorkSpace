097be3f95139073b2667dbbe13994efbae544a4a
RobertoCastro <robertoca07@hotmail.com>
2024-06-11 23:55:56 -0500

    Implementación de la funcion diferencia media en ensamblador y DSP

--- AdM-TP1/Core/Inc/data.h
+++ AdM-TP1/Core/Inc/data.h
@@ -11,7 +11,7 @@
 #include <stdint.h>
 
 #define c_potencia_POTENCIA_LEN     ((uint32_t)4)
-#define c_medDif_longitud    		((uint16_t)5)
+#define c_medDif_longitud    		((uint16_t)8)
 #define c_eco_longitud    			((uint32_t)4096)
 
 /* c_potencia data */
@@ -27,14 +27,17 @@ int16_t c_potencia_vecIn[] = {
 /* c_medDif data */
 
 int8_t c_medDif_e [c_medDif_longitud];
-int8_t c_medDif_x [] = {1, 2, 3, 4, 5};
-int8_t c_medDif_y[] = {6, 7, 8, 9, 10};
+int8_t c_medDif_x [] = {5, 9, 15, 22, 2, 4, 1, 9};
+int8_t c_medDif_y[] = {1, 3, 5, 10, 1, 8, 3, 1};
 
 int8_t asm_medDif_e [c_medDif_longitud];
 int8_t asm_DSP_medDif_e [c_medDif_longitud];
 
-/* c_medDif data */
+/* c_eco data */
 
-
+int16_t c_eco_signalIn[c_eco_longitud];
+int16_t c_eco_signalOut [c_eco_longitud];
+int16_t asm_eco_signalOut [c_eco_longitud];
+int16_t asm_DSP_eco_signalOut [c_eco_longitud];
 
 #endif /* __DATA_H__ */
